567460945f507d1370a2d4a612816a25f53262b8
ikeemthedream <55724332+ikeemthedream@users.noreply.github.com>
2024-04-13 15:05:54 -0400

    Setup Files and initial Button Interaction

---
+++ Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
+{
+    [SerializeField]
+    GameObject button;
+
+    Color originalColor;
+
+    Image image;
+
+    bool isHoveringButton = true;
+    bool isPressingingButton = true;
+
+    void Start()
+    {
+        InitialSetup();
+    }
+
+    #region Setups
+    private void InitialSetup()
+    {
+        image = button.GetComponent<Image>();
+
+        originalColor = image.color;
+    }
+    void ButtonWhenHovered()
+    {
+        image.color = Color.cyan;
+    }
+    public void ButtonWhenPressed()
+    {
+        image.color = Color.magenta;
+    }
+    #endregion
+    #region Pointer Functions
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        ButtonWhenHovered();
+        isHoveringButton = true;
+    }
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        image.color = originalColor;
+        isHoveringButton = false;
+
+        if (isPressingingButton == true)
+        {
+            ButtonWhenPressed();
+        }
+        else
+        {
+            image.color = originalColor;
+        }
+    }
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        if (isHoveringButton == false)
+        {
+            image.color = originalColor;
+        }
+        else
+        {
+            ButtonWhenHovered();
+        }
+
+        isPressingingButton = false;
+    }
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        ButtonWhenPressed();
+        isPressingingButton = true;
+    }
+    #endregion
+}
